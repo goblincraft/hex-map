b3f6080fd852a0fcf1c5063dbb959af4ef43a3b1
goblincraft <goblin.software.co@gmail.com>
2025-08-28 18:34:13 -0400

    implement draw hex function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Errors } from "./enums/Errors";
-import { HexOptions } from "./interfaces/IHexOptions";
+import { DefaultHexOptions, HexOptions } from "./interfaces/IHexOptions";
 
 export const HexGrid = (() => {
 
     let _canvas: HTMLCanvasElement;
     let _ctx: CanvasRenderingContext2D;
+    let _hexOptions: HexOptions;
 
     function _setupCanvas(el: HTMLCanvasElement) {
         _canvas = el;
@@ -13,10 +14,42 @@ export const HexGrid = (() => {
         _ctx = <CanvasRenderingContext2D>_canvas.getContext("2d");
     }
 
+    function _drawHex(x: number, y: number) {
+        _ctx.beginPath();
+        for (let i = 0; i < 6; i++) {
+            const angle = Math.PI / 3 * i;
+            _ctx.lineTo(x + _hexOptions.radius * Math.cos(angle), y + _hexOptions.radius * Math.sin(angle));
+        }
+        _ctx.closePath();
+        _ctx.fill();
+        _ctx.stroke();
+    }
+
+    function _drawGrid() {
+        
+    }
+
+    function _setCanvasStyling() {
+        if (_ctx) {
+            _ctx.fillStyle = _hexOptions.fillColor ? _hexOptions.fillColor : "transparent";
+            _ctx.strokeStyle = _hexOptions.borderColor;
+            _ctx.lineWidth = _hexOptions.borderSize;
+            _ctx.save();
+        }
+    }
+
+    function _setHexOptions(options: HexOptions) {
+        _hexOptions = options;
+        _hexOptions.height = Math.sqrt(3) * options.radius;
+        _hexOptions.width = 2 * options.radius;
+    }
+
     function draw(id: string, dotOptions?: HexOptions): void {
         const el = document.getElementById(id);
         if (el && el instanceof HTMLCanvasElement) {
             _setupCanvas(el);
+            _setHexOptions(dotOptions ? dotOptions : DefaultHexOptions);
+            _setCanvasStyling();
         } else {
             throw Error(Errors.NO_CANVAS);
         }
--- src/interfaces/IHexOptions.ts
+++ src/interfaces/IHexOptions.ts
@@ -1,5 +1,7 @@
 export interface HexOptions {
     radius: number;
+    height: number;
+    width: number;
     borderSize: number;
     borderColor: string;
     fillColor: string | null;
@@ -7,6 +9,8 @@ export interface HexOptions {
 
 export const DefaultHexOptions: HexOptions = {
     radius: 10,
+    height: 0,
+    width: 0,
     borderSize: 1,
     borderColor: "#000000",
     fillColor: "#FFFFFF"
